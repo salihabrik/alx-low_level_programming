1bc7fb5cd25ec9f87d03ac4a1705c746097576e0
salihabrik <briksaliha@gmail.com>
2023-03-17 14:25:33 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this is the main funchtion
+ * Dvariablel n each time it is executed.
+ * its stord in n is pos
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+putchar('\n');
+return (0);
+}
