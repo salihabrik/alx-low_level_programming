e7ea0284de89435fb713233986eb99e777621d17
salihabrik <briksaliha@gmail.com>
2023-03-17 14:53:03 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this is the main funchtion
+ * Dvariablel n each time it is executed.
+ * its stord in n is pos
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char chr = 'z';
+while (chr >= 'a')
+{
+putchar(chr);
+chr--;
+}
+putchar('\n');
+return (0);
+}
