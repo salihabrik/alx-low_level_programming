369323c7e043971cabf789a5f3cf92fc8ba6177b
salihabrik <briksaliha@gmail.com>
2023-03-17 13:37:12 -0700

    print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this is the main funchtion
+ * Dvariablel n each time it is executed.
+ * its stord in n is pos
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char u_chr = 'A';
+char chr = 'a';
+while (chr <= 'z')
+{
+putchar(chr);
+chr++;
+}
+while (u_chr <= 'Z')
+{
+putchar(u_chr);
+u_chr++;
+}
+putchar('\n');
+return (0);
+}
