4168d8e4f6c91a76353ffa50e352dfde8aa1fd66
salihabrik <briksaliha@gmail.com>
2023-03-21 09:57:07 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: 'points _putchar to stdout'
+ * Description: 'prints _putchar to stdout'
  * Return: always 0
  */
 int main(void)
-{
+	{
         _putchar('_');
         _putchar('p');
         _putchar('u');
@@ -15,6 +15,6 @@ int main(void)
         _putchar('a');
         _putchar('r');
         _putchar('\n');
-
-	return (0);
-}	
+	 return (0);
+	}
+  
