f8b269123bef470d99bec6ad8a92178ccad97c24
salihabrik <briksaliha@gmail.com>
2023-03-30 15:09:55 -0700

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -26,9 +26,30 @@ for (k = 0; k <= s; k++)
 if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 putchar(c[l * 10 + k]);
 else
+putchar('.');
 }
-putchar('\n');
 }
-if (size == 0)
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 10 - 1, i);
+}
 putchar('\n');
 }
