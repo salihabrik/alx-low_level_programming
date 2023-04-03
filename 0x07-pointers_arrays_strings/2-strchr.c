36a8e0ad01b214f7e459720abf78e849fc80dfea
salihabrik <briksaliha@gmail.com>
2023-04-03 15:12:22 -0700

    m

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,17 +5,17 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-        {
-		return s;
-        }
+		{
+		return (s);
+		}
 		s++;
 	}
 
 	if (c == '\0')
 	{
-        return s;
+		return (s);
 	}
 
-	return NULL;
+	return (NULL);
 }
 
