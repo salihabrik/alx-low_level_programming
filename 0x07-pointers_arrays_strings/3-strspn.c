23ce54d89af24d82a945efd01de91d7d16248cae
salihabrik <briksaliha@gmail.com>
2023-04-04 14:58:52 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,21 +10,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int _strspn(char *s, char *accept) {
+unsigned int _strspn(char *s, char *accept)
+	{
 	unsigned int i, j;
 	unsigned int count = 0;
-    for (i = 0; s[i] != '\0'; i++) {
-        for (j = 0; accept[j] != '\0'; j++) {
-            if (s[i] == accept[j]) {
-                count++;
-                break;
-            }
-        }
-        if (accept[j] == '\0') {
-            /* s[i] is not in accept */
-            break;
-        }
-    }
-    return count;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; accept[j] != '\0'; j++)
+		{
+		if (s[i] == accept[j])
+		{
+			count++;
+			break;
+		}
+		}
+		if (accept[j] == '\0')
+		{
+		break;	
+		}
+	}
+		return count;
 }
 
