2f5acc336c31e8744ce9e81f24a74a6df1792566
salihabrik <briksaliha@gmail.com>
2023-04-05 15:34:09 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string using recursion
+ * @s: The string to be printed
+ * return 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+
+		putchar(*s);
+		_puts_recursion(s + 1);
+}
+
