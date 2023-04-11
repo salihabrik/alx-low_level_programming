83f38774c10ff3a07e032758cbac0162b6d7d694
salihabrik <briksaliha@gmail.com>
2023-04-11 13:44:47 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ * Return: pointer to the copied string (Success), Null
+ **/
+char *_strdup(char *str); 
+{
+        char *k;
+
+        int i, n = 0;
+
+        if (str == NULL)
+                return (NULL);
+
+        while (str[n] != '\0')
+                n++;
+
+        k = (char *)malloc((sizeof(char) * n) + 1);
+        if (k == NULL)
+                return (NULL);
+
+        for (i = 0; i < n; i++)
+                k[i] = str[i];
+        k[n] = '\0';
+
+        return (k);
+}
