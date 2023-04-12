e824c1645776488d1c8fceaac165fe42e3cbf770
salihabrik <briksaliha@gmail.com>
2023-04-12 13:27:30 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,20 +1,40 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- *_strlen - count arrray
- *@s: array of elements
- *Return: i
+ * str_concat - creating an array contans another str
+ * @s1: second str
+ * @s2: first str
+ *
+ * Return: allocated str that contains both.
  */
 
 char *str_concat(char *s1, char *s2)
 {
+	unsigned int len1 = s1 ? strlen(s1) : 0;
+	unsigned int len2 = s2 ? strlen(s2) : 0;
+	char *result = malloc(len1 + len2 + 1);
 	unsigned int i;
 
-	i = 0;
-	while (s[i] != '\0') /*
+	if (!result)
 	{
-		i++;
+		return (NULL);
 	}
-
-	return (i);
+	if (s1)
+	{
+		for (i = 0; i < len1; i++)
+		{
+			result[i] = s1[i];
+		}
+	}
+	if (s2)
+	{
+		for (i = 0; i < len2; i++)
+		{
+			result[len1 + i] = s2[i];
+		}
+	}
+	result[len1 + len2] = '\0';
+	return (result);
 }
-
