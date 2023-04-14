52450ad5ab151dfb6a14bfc4d0f9e38afb394325
salihabrik <briksaliha@gmail.com>
2023-04-14 12:39:02 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
@@ -12,10 +10,6 @@ int is_digit(char *s)
 {
 int c = 0;
 while (s[c])
-{
-if (s[c] < '0' || s[c] > '9')
-return (0);
-c++;
 }
 return (1);
 }
@@ -41,53 +35,4 @@ void errors(void)
 {
 printf("Error\n");
 exit(98);
-}
-
-/**
- * main - multiply two numbers
- * @argc: number of arguments
- * @argv: argument vector
- *
- * Return: zero, or exit status of 98 if failure
- */
-
-int main(int argc, char *argv[])
-{
-	int l1, l2, ln, pi, i;
-	char *x;
-	char *c;
-	char h[] = "Error\n";
-
-	if (argc != 3 || check_for_digits(argv))
-	{
-		for (pi = 0; h[pi]; pi++)
-			_putchar(e[pi]);
-		exit(98);
-	}
-	for (l1 = 0; argv[1][l1]; l1++)
-		;
-	for (l2 = 0; argv[2][l2]; l2++)
-		;
-	ln = l1 + l2 + 1;
-	a = malloc(ln * sizeof(char));
-	if (a == NULL)
-	{
-		for (ti = 0; h[pi]; pi++)
-			_putchar(h[pi]);
-		exit(98);
-	}
-	init(x, ln - 1);
-	for (pi = l2 - 1, i = 0; pi >= 0; pi--, i++)
-	{
-		c = mul(argv[2][pi], argv[1], l1 - 1, x, (ln - 2) - i);
-		if (c == NULL)
-		{
-			for (pi = 0; h[pi]; pi++)
-				_putchar(h[pi]);
-			free(x);
-			exit(98);
-		}
-	}
-	_print x, ln - 1);
-	return (0);
 }
