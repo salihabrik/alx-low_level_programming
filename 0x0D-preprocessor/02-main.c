b359299afe821654220a6f5fb65550703988197f
salihabrik <briksaliha@gmail.com>
2023-04-17 10:22:08 -0700

    non one care

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - print the name of the file it was compiled from
+ * main - check the code
  *
- *Return:0(success)
- *
+ * Return: Always 0.
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-
