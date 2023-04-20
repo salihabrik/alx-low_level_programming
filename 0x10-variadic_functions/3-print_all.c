5166eb7d803056cdf120d039b96166a95ae854e7
salihabrik <briksaliha@gmail.com>
2023-04-20 06:58:51 -0700

    One woman's constant is another woman's variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - print anything
+ * @format: format of the input
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list param_list;
+	char *separator, *str;
+	int x;
+
+	separator = "";
+	x = 0;
+
+	va_start(param_list, format);
+
+	if (format)
+	{
+		while (format[x])
+		{
+			switch (format[x])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(param_list, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(param_list, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(param_list, double));
+					x++;
+					continue;
+			}
+			separator = ", ";
+			x++;
+		}
+	}
+	printf("\n");
+	va_end(param_list);
+}
