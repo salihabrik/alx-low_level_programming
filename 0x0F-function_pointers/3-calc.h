d563b3d0c428f22b4e61e45c0a93afd7c57e2d9e
salihabrik <briksaliha@gmail.com>
2023-04-21 06:29:22 -0700

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,22 +1,21 @@
 #ifndef CALC_H
 #define CALC_H
+int _putchar(char c);
 /**
- * struct op - A struct op.
- * @op: The operator.
- * @f: The associated function.
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-
 } op_t;
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
-#endif
+#endif /* CALC_H */
