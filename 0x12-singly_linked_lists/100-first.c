62a626a3b544d36a7e3329ecffd0b9748fe57cb3
salihabrik <briksaliha@gmail.com>
2023-04-27 15:53:57 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * function that prints You're beat! 
+ * and yet, you must allow,
+ * nI bore my house upon my back!\n
+ * before the main function is executed
+ *
+ * RETURN 0
+ **/
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+	return 0;
+}
