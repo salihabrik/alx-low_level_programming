2246405b38ab50ba6564d980283cdb96f6487570
salihabrik <briksaliha@gmail.com>
2023-04-29 12:26:17 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,29 @@
 #include "main.h"
+
 /**
- * _sqrt_helper - auxiliary function to calculate square root recursively
- * @n: the number to calculate the square root of
- * @min: the minimum possible value of the square root
- * @max: the maximum possible value of the square root
+ * check - checks for the square root
+ * @a:int
+ * @b:int
  *
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * Return: int
  */
-int _sqrt_helper(int n, int min, int max)
+int check(int a, int b)
 {
-    if (min > max)
-        return (-1);
-
-    int mid = (min + max) / 2;
-    int mid_squared = mid * mid;
-
-    if (mid_squared == n)
-        return (mid);
-    else if (mid_squared < n)
-        return (_sqrt_helper(n, mid + 1, max));
-    else
-        return (_sqrt_helper(n, min, mid - 1));
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (check(a + 1, b));
 }
 
 /**
- * _sqrt_recursion - calculate the square root of a number using recursion
- * @n: the number to calculate the square root of
- *
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ * Return: natural square root or -1
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-
-    return (_sqrt_helper(n, 0, n));
+	if (n == 0)
+		return (0);
+	return (check(1, n));
 }
-
