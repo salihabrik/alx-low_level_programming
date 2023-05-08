909e4c295c484ed4310cda9531c5bd71fc54d0a5
salihabrik <briksaliha@gmail.com>
2023-05-08 15:50:57 -0700

    Speak gently, she can hear

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,23 +11,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	int f, i, count = 0;
 
+	if (filename == NULL)
+		return (-1);
+
+
+	f = creat(filename, 0600);
 	if (f == -1)
+		return (-1);
+
+	if (text_content)
 	{
-	return (-1);
+		while (text_content[count])
+			count++;
+
+		i = write(f, text_content, count);
+		if (i == -1)
+		{
+			return (-1);
+		}
 	}
 
-	if (text_content != NULL)
-	{
-	int count = write(f, text_content, strlen(text_content));
-
-	if (count == -1)
-	{
-	close(f);
-	return (-1);
-	}
-	}
-	close(f);
-	return (0);
+	return (1);
 }
+
+
