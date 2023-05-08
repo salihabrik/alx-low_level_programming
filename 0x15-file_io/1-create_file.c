a674af4fdf54a2ac9b2b3a165f8f454435123618
salihabrik <briksaliha@gmail.com>
2023-05-08 15:45:19 -0700

    Speak gently, she can hear

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	int f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (f == -1)
 	{
