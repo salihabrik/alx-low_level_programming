731d7d5d53d00f4e334fc9a1f9820181df939028
salihabrik <briksaliha@gmail.com>
2023-05-09 15:22:12 -0700

    Tread lightly, she is near

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,32 +10,32 @@
 
 int main(int argc, char *argv[])
 {
-        int f_r, f_w, r, a, b;
-        char buf[BUFSIZ];
+	int f_r, f_w, r, a, b;
+	char buf[BUFSIZ];
 
-        if (argc != 3)
-        {
-                dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-                exit(97);
-        }
-        f_r = open(argv[1], O_RDONLY);
-        if (f_r < 0)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-                exit(98);
-        }
-        f_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-        while ((r = read(f_r, buf, BUFSIZ)) > 0)
-        {
-                if (f_w < 0 || write(f_w, buf, r) != r)
-                {
-                        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-                        close(f_r);
-                        exit(99);
-                }
-        }
-        if (r < 0)
-        {
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	f_r = open(argv[1], O_RDONLY);
+	if (f_r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	f_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(f_r, buf, BUFSIZ)) > 0)
+	{
+		if (f_w < 0 || write(f_w, buf, r) != r)
+		{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(f_r);
+			exit(99);
+		}
+	}
+	if (r < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
