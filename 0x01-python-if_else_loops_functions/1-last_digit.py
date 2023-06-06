4c1a66e4c49562b42f34e8b33b2e5c7883fbd235
salihabrik <briksaliha@gmail.com>
2023-06-06 16:16:34 -0700

    . I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
-if last_digit > 5:
-    print(f"Last digit of {number:d} is {number % n:d}and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number:d} is {number % n:d}"and is 0")
+last_dgit = abs(number) % 10
+if number < 0:
+    last_dgit = -last_dgit
+    
+if last_dgit > 5:
+    print(f"Last digit of {number} is {last_dgit} and is greater than 5")
+elif last_dgit < 6 and last_dgit != 0:
+    print(f"Last digit of {number} is {last_dgit} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number:d} is \
-{number % n:d} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_dgit} and is 0")
