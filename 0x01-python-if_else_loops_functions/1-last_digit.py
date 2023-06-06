91633cc4e2526271ebda8cd60dd55e304727ec2b
salihabrik <briksaliha@gmail.com>
2023-06-06 16:36:05 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_dgit = abs(number) % 10
+last_dgt = abs(number) % 10
 if number < 0:
-    last_dgit = -last_dgit
+    last_dgt = -last_dgt
     
-if last_dgit > 5:
-    print(f"Last digit of {number} is {last_dgit} and is greater than 5")
-elif last_dgit < 6 and last_dgit != 0:
-    print(f"Last digit of {number} is {last_dgit} and is less than 6 and not 0")
+if last_dgt > 5:
+    print(f"Last digit of {number} is {last_dgt} and is greater than 5")
+elif last_dgt < 6 and last_dgt != 0:
+    print(f"Last digit of {number} is {last_dgt} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {last_dgit} and is 0")
+    print(f"Last digit of {number} is {last_dgt} and is 0")
