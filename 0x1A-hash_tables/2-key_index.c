28f786485c086e2e148325ee1bb46a1f202d5628
salihabrik <briksaliha@gmail.com>
2023-07-07 08:20:46 -0700

    Simplicity and elegance are unpopular because they require hard work and discipline to achieve and education to be appreciated

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,12 @@
+#include "hash_tables.h"
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_value;
 	unsigned long int index;
 
-	/* Get the hash value using the hash_djb2 function */
 	hash_value = hash_djb2(key);
 
-	/* Compute the index based on the hash value and size of the array */
 	index = hash_value % size;
 
 	return index;
